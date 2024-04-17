838900025c796cea7555a9f7573eeef3d9f625e8
zhan0858 <zhan0858@algonquinlive.com>
2024-04-17 17:43:44 -0400

    add jwt decode token and get current user's id

--- app/crap/page.js
+++ app/crap/page.js
@@ -1,7 +1,20 @@
 // export const revalidate = false; // means cache forever
 export const revalidate = 0; // means cache for 10 mins
+import jwt from 'jsonwebtoken';
 import { getSession } from '@/app/actions';
 import { cookies } from 'next/headers';
+import Link from 'next/link';
+
+
+const decodeToken = (token) => {
+	try {
+		const decoded = jwt.decode(token, { complete: true });
+		return decoded;
+	} catch (error) {
+		console.error('Error decoding token:', error);
+		return null;
+	}
+};
 
 export default async function Page({ params, searchParams }) {
 	cookies();
@@ -14,21 +27,27 @@ export default async function Page({ params, searchParams }) {
 	const data = await response.json();
 	const list = data?.data;
 	console.log('page data =', data);
+	const myId = decodeToken(token?.value)?.payload?.id;
+	// console.log('myId =', myId);
 
 	return (
 		<main className="flex min-h-screen flex-col items-center py-12">
 			<div className="w-full mt-6 px-12">
-				<h2 className="text-center text-xl">There are 6
+				<h2 className="text-center text-xl">There are
+					<span className="font-bold mx-2">{list.length > 0 ? list.length : '0'}</span>
 					<span className="text-yellow-500 mx-2 font-bold">AVAILABLE</span> matches  {keyword ? 'for ' + keyword : 'in ' + distance + 'm'}</h2>
 			</div>
 			<div className="w-full p-3 flex flex-col gap-3">
 				{
-					list && list.length > 0 && list.map((item, index) => {
+					list && list.length > 0 && list.map(item => {
 						return (
-							<div className="bg-primary-dark text-white rounded-md w-full p-3">
-								<h3>{item.title}</h3>
+							<Link key={item._id} href={`/crap/${item._id}`} className="bg-primary-dark text-white rounded-md w-full p-3">
+								<h3 className="text-xl uppercase">{item.title}</h3>
 								<p>{item.description}</p>
-							</div>
+								{
+									item.owner._id == myId && <p className="text-yellow-400 font-bold">This is your own crap</p>
+								}
+							</Link>
 						)
 					})
 				}
