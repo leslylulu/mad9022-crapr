ecd84b9c07033e13133698c0fce0d9d6b3eba3b9
zhan0858 <zhan0858@algonquinlive.com>
2024-04-18 19:17:02 -0400

    revalidatePath path

--- app/actions.js
+++ app/actions.js
@@ -31,6 +31,7 @@ export async function createCrap(fd) {
 	if (!response.ok) {
 		console.log('createCrap failed', response.status);
 	} else {
+		revalidatePath('/mine');
 		redirect('/mine');
 	}
 }
