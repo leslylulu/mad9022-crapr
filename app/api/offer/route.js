79b792c2aae8011b50b90c7a977e5bd24e2badf4
zhan0858 <zhan0858@algonquinlive.com>
2024-04-19 02:10:55 -0400

    change region

--- app/api/offer/route.js
+++ app/api/offer/route.js
@@ -1,7 +1,7 @@
 export const revalidate = 0
 export const dynamic = 'force-dynamic';
 export const runtime = 'edge'; // 'nodejs' is the default
-export const preferredRegion = ['iad1'];
+export const preferredRegion = ['cle1'];
 
 
 const { NEXT_API_URL } = process.env;
